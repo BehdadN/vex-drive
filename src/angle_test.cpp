f7475a6024159d370d6a9968139e3829cca13ea5
Behdad Nikfarjam <behdadn500@gmail.com>
2021-11-19 15:52:31 +0100

    hehehehehe

---
+++ src/angle_test.cpp
@@ -0,0 +1,6 @@
+#include "angle_test.h"
+#include "vex.h"
+
+void atest() {
+  Drivetrain.turnFor(right, 90, degrees);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -19,35 +19,13 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include "angle_test.h"
+#include "drive.h"
 
 using namespace vex;
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  while (true) {
-    // vision: 316 pixels across, 212 down !!!
-    Brain.Screen.clearScreen();
-    Brain.Screen.setCursor(1, 1);
-
-    if (BumperA.pressing() && BumperB.pressing()) {
-      // if we are holding mobile goal
-      
-
-      Vision20.takeSnapshot(Vision20__DONUT);
-
-      // vision20 takes picture
-
-      if (Vision20.objects[0].exists) {
-        // if the donut is there, print the x value of center
-        Brain.Screen.print(Vision20.objects[0].centerX);
-      } else {
-        Brain.Screen.print("nothing detected :(");
-      }
-    } else {
-      Brain.Screen.print("no mobile goal");
-    }
-    wait(1, seconds);
-  }
+  atest();
 }
